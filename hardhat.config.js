1e3e5bd8978770f0dea94a13a8c10e0cd4d68406
hamzaaziz1 <hamzaaziz5874@gmail.com>
2023-08-30 11:37:44 +0500

    .

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and replace "KEY" with it
-const INFURA_API_KEY = "980e625cbc534e02a2265deeb8de0b08";
+const INFURA_API_KEY = "your infura api key";
 
 // Replace this private key with your Sepolia account private key
 // To export your private key from Coinbase Wallet, go to
@@ -10,7 +10,7 @@ const INFURA_API_KEY = "980e625cbc534e02a2265deeb8de0b08";
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = "d6cc85c6a284db1f668ab81667c2f9bd9ed00cb5d52be916e6f0e965a5233ef2";
+const SEPOLIA_PRIVATE_KEY = "meta mask private key";
 
 module.exports = {
   solidity: "0.8.19",
